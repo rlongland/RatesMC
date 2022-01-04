0e8f0bea7ec623a2ed970affbc49e0eecb3b33ed
Richard Longland <richard_longland@ncsu.edu>
2022-01-04 10:54:27 -0500

    Rearrange code

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((tab-width . 8)
+	 (indent-tabs-mode . t)))
+ (c++-mode . ((c-basic-offset . 2)
+          (tab-width . 2)
+          (indent-tabs-mode . t)
+          (compile-command . "make -C ../build -j 2"))))
+
+((nil . ((company-clang-arguments . ("-Iinclude/")))))
---
+++ src/.dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((tab-width . 8)
+	 (indent-tabs-mode . t)))
+ (c++-mode . ((c-basic-offset . 2)
+          (tab-width . 2)
+          (indent-tabs-mode . t)
+          (compile-command . "make -C ../../build -j 2"))))
+
