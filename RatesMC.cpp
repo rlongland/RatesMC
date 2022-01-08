9e4ba8433d03b53cae63a9395d19909a9149b59d
Richard Longland <richard_longland@ncsu.edu>
2022-01-07 21:27:27 -0500

    Added licences to source code

--- RatesMC.cpp
+++ RatesMC.cpp
@@ -1,18 +1,24 @@
-/* ======================================================================
-   RatesMC
-   Author: R. Longland
-   Start Date: 2018-11-13
-
-   Description: Monte Carlo reaction rate code. This version 2 is for
-   public release!
-
-   TO DO:
-	 - Interfering resonances
-	 - Error accounting output
-	 - nan accounting
-	 - Output non-resonant s-factor?
-   ======================================================================
-*/
+/* RatesMC: Monte Carlo reaction rate code
+ *
+ * Copyright (C) 2022  Richard Longland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: R. Longland
+ *  Email: rllongla@ncsu.edu
+ *
+ */
 
 #include <iostream>
 #include <iomanip>
@@ -236,10 +242,10 @@ int main(int argc, char** argv){
 void WelcomeScreen(){
   std::cout << std::endl;
   std::cout << " ********************************************" << std::endl;
-  std::cout << " *           Welcome to RatesMC             *" << std::endl;
+  std::cout << " *                 RatesMC                  *" << std::endl;
+	std::cout << " *      Copyright (C) 2022  R. Longland     *" << std::endl;
   std::cout << " *         V. " << VersionNumber << "  " << VersionDate 
 						<< "        *" << std::endl;
-  std::cout << " *     Written by:   Richard Longland       *" << std::endl;
   std::cout << " *                                          *" << std::endl;
   std::cout << " ********************************************" << std::endl;
   std::cout << "\n" << std::endl;
