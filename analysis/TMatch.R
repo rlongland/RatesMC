2e72067c64dbbb82d720c5430a0742b940075a11
Richard Longland <richard_longland@ncsu.edu>
2022-08-24 14:23:49 +0100

    Calculates narrow resonance rates

--- analysis/TMatch.R
+++ analysis/TMatch.R
@@ -12,10 +12,14 @@ ContributionFile <- "RatesMC.cont"
 HFFile           <- "Tmatch.HF"
 
 OutputFile       <- "TMatch.out"
+LaTeXFile        <- "TMatch.latex"
+
+## Read the contributions file? (otherwise use narrow resonances)
+ReadCont         <- TRUE
 
 ## The parameter, n, to determine maximum of Gamow peak
 ## (see Newton paper, Eqn. 3)
-n.Gamow <- 1
+n.Gamow          <- 1
 
 ######################################################################
 ## You shouldn't need to touch anything below this line...
@@ -44,8 +48,8 @@ mypdf <- function(file="output.pdf",...)
 
 
 ## Make sure the RatesMC.in title matches the Tmatch.HF one
-title <- scan(RatesMCFile,what=character(),n=1)
-titleHF <- scan(HFFile,what=character(),n=1)
+title <- scan(RatesMCFile,what=character(),n=1,quiet=TRUE)
+titleHF <- scan(HFFile,what=character(),n=1,quiet=TRUE)
 
 if(title != titleHF){
     stop("\n",
@@ -54,9 +58,6 @@ if(title != titleHF){
     ##stop()
 }
   
-## Read in the contribution file
-data <- read.table(ContributionFile,skip=1,header=FALSE)
-
 ## Read and format the reaction name
 ReacName = as.character(read.table(RatesMCFile,skip=0,header=FALSE,nrows=1)$V1)
 # Make superscripts, subscripts etc
@@ -70,46 +71,103 @@ ReacName <- sub("\\)","\\)*",ReacName)
 ReacName <- gsub("([[:digit:]]+)", "phantom()^{\\1}*", ReacName)
 
 ## Read the masses and charges
-Z0 <- scan(RatesMCFile,what=numeric(),skip=2,n=1)
-Z1 <- scan(RatesMCFile,what=numeric(),skip=3,n=1)
-M0 <- scan(RatesMCFile,what=numeric(),skip=5,n=1)
-M1 <- scan(RatesMCFile,what=numeric(),skip=6,n=1)
+Z0 <- scan(RatesMCFile,what=numeric(),skip=2,n=1,quiet=TRUE)
+Z1 <- scan(RatesMCFile,what=numeric(),skip=3,n=1,quiet=TRUE)
+M0 <- scan(RatesMCFile,what=numeric(),skip=5,n=1,quiet=TRUE)
+M1 <- scan(RatesMCFile,what=numeric(),skip=6,n=1,quiet=TRUE)
+J0 <- scan(RatesMCFile,what=numeric(),skip=8,n=1,quiet=TRUE)
+J1 <- scan(RatesMCFile,what=numeric(),skip=9,n=1,quiet=TRUE)
+mu <- M0*M1/(M0+M1)
 
-## Read in the resonance energies.
-## This procedure should read in all resonance energies without erroring!
-skip <- 29
-Energies <- numeric()
-while(
-  ## Try to read a line and see if an actual number was read.
-  substr(x <- try(unlist(read.table(RatesMCFile,skip=skip,nrows=1)[1]
-                             )),1,2) != "**"){
-  ## append the resonance energy list and skip to the next line
-  skip <- skip+1
-  if(is.numeric(x))
-    Energies <- c(Energies,x)
+## The RatesMC rates
+Rates <- read.table(RatesMCOutput,skip=4)
+T <- Rates[,1]
+
+
+if(ReadCont){
+    ## Read in the resonance energies.
+    ## This procedure should read in all resonance energies without erroring!
+    skip <- 29
+    Energies <- numeric()
+    while(
+	## Try to read a line and see if an actual number was read.
+	substr(x <- try(unlist(read.table(RatesMCFile,skip=skip,nrows=1)[1]
+			       )),1,2) != "**"){
+				   ## append the resonance energy list and skip to the next line
+				   skip <- skip+1
+				   if(is.numeric(x))
+				       Energies <- c(Energies,x)
+			       }
+    ## Ignore Upper limit resonances for this calculation?
+    ## Now read the upper limit resonances in the same way
+    skip <- skip+5
+    while(
+	substr(x <- try(unlist(read.table(RatesMCFile,skip=skip,nrows=1)[1]
+			       )),1,2) != "**"){
+				   skip <- skip+1
+				   if(is.numeric(x))
+				       Energies <- c(Energies,x)
+			       }
+    ##Energies <- format(Energies,digits=1,trim=TRUE)
+
+    ## Read in the contribution file
+    data <- read.table(ContributionFile,skip=1,header=FALSE)
+
+    ## Look for resonances that contribute more than 5% in the specified
+    ## temperature range
+    ## Look only at the centroids
+    MedSeq <- seq(from=9,to=length(data[1,]),by=3)
+    resCont <- data[,MedSeq]
+
+} else {
+
+    ## Read all resonances from the RatesMC input file
+    data <- numeric()
+    skip <- 30
+    ## Keep looping until we're done
+    while(TRUE){
+	## Read a line
+	x <- try(read.table(RatesMCFile,skip=skip,nrows=1))
+	## Break the loop if we see *s
+	if(substr(x[,1],1,2) == "**") break
+	## Is the first entry a '+' or '!'? Ignore if so
+	if(x[1] == "+" || x[1] == "!") {
+	    skip <- skip+1
+	    next
+	}
+	## Otherwise pull the information
+	data <- rbind(data,x[c(1,3,5,6,9,12)])
+
+	## Move onto next line and start again!
+	skip <- skip+1
+    }
+
+    ## The energies and resonance strengths
+    Energies <- data[,1]
+    ## If resonance strength is unknown, calculate it from the given partial widths
+    wgUnknown <- data[,2]==0
+    w <- (2*data[,3]+1)/((2*J0 + 1)*(2*J1 + 1))
+    G1 <- data[,4]
+    G2 <- data[,5]
+    GT <- G1+G2+data[,6]
+    wg <- w*G1*G2/GT
+    ## Then back-fill the unknown values
+    data[wgUnknown,2] <- wg[wgUnknown]
+    ## And grab it all back into a convenient vector
+    wg <- data[,2]
+
+    ## Now that we know the resonance energies and strengths, the rate
+    ## can be calculated for each resonance at each temperature
+    allRate <- sapply(T,function(Ti)(1.5399e11/(mu*Ti)^(1.5))*wg*1e-6*exp(-0.011605*Energies/Ti))
+    resCont <- t(apply(allRate,2,function(t)t/sum(t)))
+
+    ##resCont[is.nan(resCont)] <- NA
 }
-## Ignore Upper limit resonances for this calculation?
-## Now read the upper limit resonances in the same way
-skip <- skip+5
-while(
-  substr(x <- try(unlist(read.table(RatesMCFile,skip=skip,nrows=1)[1]
-                             )),1,2) != "**"){
-  skip <- skip+1
-  if(is.numeric(x))
-    Energies <- c(Energies,x)
-}
-##Energies <- format(Energies,digits=1,trim=TRUE)
+
 ## The maximum energy
 maxE <- max(Energies)
 
-
-## Look for resonances that contribute more than 5% in the specified
-## temperature range
-## Look only at the centroids
-MedSeq <- seq(from=9,to=length(data[1,]),by=3)
-T <- data[,1]
-resCont <- data[,MedSeq]
-
+## A function to calculate the Gamow peak and width, and plot it if desired!
 Gamow <- function(Temp, plot=FALSE, ymax=1){
     kT <- 0.086171*Temp
     somer <- 0.989534*Z0*Z1*sqrt(M0*M1/(M0+M1))
@@ -163,23 +221,32 @@ for(i in 1:length(T)){
     cont <- as.numeric(resCont[i,])[o]   ## These are the individual contributions
 
     ## Now do a linear interpolation. cubic spline acts too crazy!
-    interp.ETER <- approxfun(y=Energies.o,x=cumsum(cont/sum(cont)))
-    ETER <- interp.ETER(c(0.08,0.5,0.92))
-    ETER.array <- c(ETER.array,ETER[2] + (ETER[3]-ETER[1]))
+    csum <- cumsum(cont/sum(cont))
+    if(length(unique(csum)) > 1){
+	interp.ETER <- approxfun(y=Energies.o,x=cumsum(cont/sum(cont)))
+	ETER <- interp.ETER(c(0.08,0.5,0.92))
+	ETER.array <- c(ETER.array,ETER[2] + (ETER[3]-ETER[1]))
 
-    segments(x0=ETER[1], x1=ETER[3], y0=max(resCont[i,])*0.92,
-	     col=cols[1],lwd=10,lend=1)
-    arrows(x0=ETER[2], x1=ETER[2] + (ETER[3]-ETER[1]), y0=max(resCont[i,])*0.92,
-	     col=cols[1],lwd=2,length=0.1)
-    text(x=(ETER[1]+ETER[3])/2,y=max(resCont[i,])*0.92, "ETER",adj=0.5)
-    text(x=ETER[3],y=max(resCont[i,])*0.92, "Median + ETER",pos=3)
-    ##plot(y=Energies.o,x=cumsum(cont/sum(cont)))
-    ##abline(v=c(0.08,0.5,0.92))
-    ##abline(h=interp.ETER(c(0.08,0.5,0.92)))
+	segments(x0=ETER[1], x1=ETER[3], y0=max(resCont[i,])*0.92,
+		 col=cols[1],lwd=10,lend=1)
+	arrows(x0=ETER[2], x1=ETER[2] + (ETER[3]-ETER[1]), y0=max(resCont[i,])*0.92,
+	       col=cols[1],lwd=2,length=0.1)
+	text(x=(ETER[1]+ETER[3])/2,y=max(resCont[i,])*0.92, "ETER",adj=0.5)
+	text(x=ETER[3],y=max(resCont[i,])*0.92, "Median + ETER",pos=3)
+	##plot(y=Energies.o,x=cumsum(cont/sum(cont)))
+	##abline(v=c(0.08,0.5,0.92))
+	##abline(h=interp.ETER(c(0.08,0.5,0.92)))
+    } else {
+	ETER.array <- c(ETER.array,NA)
+    }
     
 }    
 
-## Now do the interpolation
+##----------------------------------------------------------------------
+## Now do the interpolation to find the temperature where maximum energy resonance corresponds
+## to the end of the Gamow peak or effective burning energy
+cat("\n")
+
 ## First Gamow peak method
 interp.Gammow <- splinefun(x=Gamow.array, y=T)
 TMatch.Gamow <- interp.Gammow(maxE)
@@ -192,13 +259,9 @@ interp.ETER <- splinefun(x=ETER.array[cut], y=T[cut])
 TMatch.ETER <- interp.ETER(maxE)
 cat("TMatch from ETER method:       ",TMatch.ETER,"\n")
 
-##par(def.par)
-layout(matrix(c(1:2), 2, 1, byrow=TRUE))
-
 
 ######################################################################
 ## Now match the HF rate
-Rates <- read.table(RatesMCOutput,skip=4)
 HF <- read.table(HFFile, skip=3)
 
 ## Make an interpolation spline for Rates and HF Rate
@@ -222,29 +285,30 @@ HF.matched.low  <- HF[,2]*Norm.low
 HF.matched.high <- HF[,2]*Norm.high
 HF <- cbind(HF, HF.matched.low, HF.matched.med, HF.matched.high)
 
-ylim <- range(c(Rates[,2],HF[,2:5]))
-
-plot(Rates[,1], Rates[,3], type='l', ylim=ylim,
-     xlab="Temperature", ylab="Rate", col=cols[1], log="",
-     xaxs='i')
-abline(v=TMatch.ETER,lty=3,col="grey")
-lines(HF[,1],HF[,2],col=cols[2],lty=2)
-lines(HF[,1],HF[,4],col=cols[3],lty=1)
-
-
-## Now we need to interpolate thew matched HF rate at the RatesMC temperatures
+## We need to interpolate the matched HF rate at the RatesMC temperatures
 interp.HF.low  <- splinefun(HF[,1], HF[,3])
 interp.HF.med  <- splinefun(HF[,1], HF[,4])
 interp.HF.high <- splinefun(HF[,1], HF[,5])
 HF.final.low  <- interp.HF.low(Rates[,1])
 HF.final.med  <- interp.HF.med(Rates[,1])
 HF.final.high <- interp.HF.high(Rates[,1])
-## and plot!
+
+## Plot the HF and, scaled HF, and interpolated HF to check it all went well!
+layout(matrix(c(1:2), 2, 1, byrow=TRUE))
+ylim <- range(c(Rates[,2],HF[,2:5]))
+
+plot(Rates[,1], Rates[,3], type='l', ylim=ylim,
+     xlab="Temperature", ylab="Rate", col=cols[1], log="",
+     xaxs='i',main=paste("TMatch = ",format(TMatch.ETER,digits=2,nsmall=2),"GK"))
+abline(v=TMatch.ETER,lty=3,col="grey")
+lines(HF[,1],HF[,2],col=cols[2],lty=2)
+lines(HF[,1],HF[,4],col=cols[3],lty=1)
 lines(Rates[,1],HF.final.med,col=cols[4],lty=2)
 
 legend("topleft",legend=c("Rec.","HF","Matched HF", "Matched HF at RatesMC gridpoints"),
        lty=c(1,2,1,2),col=cols[c(1,2,3,4)])
 
+##----------------------------------------------------------------------
 ## Create the final rates table
 ## Logical vectors for above and below TMatch
 cut <- T<TMatch.ETER
@@ -258,6 +322,7 @@ Rates.final <- rbind(Rates.final,
 			   HF.final.low[acut], HF.final.med[acut], HF.final.high[acut],
 			   rep(Rates[cut.length,5],acut.length)))
 
+## Plot the final rate in a nice simple figure
 ylim <- range(Rates.final[,2:4])
 plot(Rates[,1], Rates[,3], type='l', ylim=ylim,
      xlab="Temperature", ylab="Rate", col=cols[1], log="",
@@ -272,7 +337,7 @@ legend("topleft",legend=c("Unmatched Rate","Matched Rate"),
 dev.off()
 
 ######################################################################
-## Write the output!
+## Finally write the output!
 
 ## Read in the header from RatesMC.out
 header <- readLines(RatesMCOutput,n=4)
@@ -281,22 +346,28 @@ writeLines(header, OutputFile)
 ## Output the Rates up to the matching temperature
 cut <- T<TMatch.ETER
 OP.T <- formatC(Rates.final[cut,1],"f",width=6, digits=3)
-OP.Low  <- formatC(Rates.final[cut,2],"e",width=12, digits=3,flag="#")
-OP.Med  <- formatC(Rates.final[cut,3],"e",width=16, digits=3,flag="#")
-OP.High <- formatC(Rates.final[cut,4],"e",width=16, digits=3,flag="#")
-OP.fu   <- formatC(Rates.final[cut,5],"e",width=17, digits=3,flag="#")
-OP <- cbind(OP.T, OP.Low, OP.Med, OP.High, OP.fu)
-write.table(OP,OutputFile,col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE, sep="")
+OP.Low  <- formatC(Rates.final[cut,2],"E",width=12, digits=3,flag="#")
+OP.Med  <- formatC(Rates.final[cut,3],"E",width=16, digits=3,flag="#")
+OP.High <- formatC(Rates.final[cut,4],"E",width=16, digits=3,flag="#")
+OP.fu   <- formatC(Rates.final[cut,5],"E",width=17, digits=3,flag="#")
+OP.below <- cbind(OP.T, OP.Low, OP.Med, OP.High, OP.fu)
+LaTeX.below <- cbind(OP.T, " & ", OP.Low, "  & ", OP.Med, "  &\n", "     ",
+		     OP.High, "  &", OP.fu, "    \\\\")
+write.table(OP.below,OutputFile,col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE, sep="")
+write.table(LaTeX.below,LaTeXFile,col.names=FALSE, row.names=FALSE, quote=FALSE, append=FALSE, sep="")
 
 ## Repeat the process above the matching temperature
 cut <- T>=TMatch.ETER
 OP.T <- formatC(Rates.final[cut,1],"f",width=6, digits=3)
-OP.Low  <- formatC(Rates.final[cut,2],"e",width=8, digits=3,flag="#")
-OP.Med  <- formatC(Rates.final[cut,3],"e",width=8, digits=3,flag="#")
-OP.High <- formatC(Rates.final[cut,4],"e",width=8, digits=3,flag="#")
-OP.fu   <- formatC(Rates.final[cut,5],"e",width=8, digits=3,flag="#")
-OP <- cbind(OP.T, "  (", OP.Low, ")", "     (", OP.Med,")", "     (", OP.High, ")","      (", OP.fu,")")
-write.table(OP,OutputFile,col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE, sep="")
+OP.Low  <- formatC(Rates.final[cut,2],"E",width=8, digits=3,flag="#")
+OP.Med  <- formatC(Rates.final[cut,3],"E",width=8, digits=3,flag="#")
+OP.High <- formatC(Rates.final[cut,4],"E",width=8, digits=3,flag="#")
+OP.fu   <- formatC(Rates.final[cut,5],"E",width=8, digits=3,flag="#")
+OP.above <- cbind(OP.T, "  (", OP.Low, ")", "       (", OP.Med,")", "     (", OP.High, ")","      (", OP.fu,")")
+LaTeX.above <- cbind(OP.T, " &   (", OP.Low, ") &       (", OP.Med, ") &\n", "           ",
+		     "(",OP.High, ") &       (", OP.fu, ")   \\\\")
+write.table(OP.above,OutputFile,col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE, sep="")
+write.table(LaTeX.above,LaTeXFile,col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE, sep="")
 
 ## Next:
 ## calculate narrow resonances
