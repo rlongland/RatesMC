ad72b9da0abf6d9c7558d76855bdf72f91ed8748
Richard Longland <richard_longland@ncsu.edu>
2022-09-27 12:42:42 +0100

    Bugfix: add.alpha definition in R scripts

--- analysis/PlotCompare.R
+++ analysis/PlotCompare.R
@@ -32,6 +32,14 @@ mypdf <- function(file="output.pdf",...)
         mgp=c(3,0.5,0),
 	family="sans")     
   }
+## Add an alpha value to a colour
+add.alpha <- function(col, alpha=1){
+  if(missing(col))
+    stop("Please provide a vector of colours.")
+  apply(sapply(col, col2rgb)/255, 2, 
+                     function(x) 
+                       rgb(x[1], x[2], x[3], alpha=alpha))  
+}
 
 library("gsl")
 
--- analysis/PlotCorrelations.R
+++ analysis/PlotCorrelations.R
@@ -27,6 +27,14 @@ mypdf <- function(file="output.pdf",...)
         tcl=0.5,
         mgp=c(3,0.5,0))     
   }
+## Add an alpha value to a colour
+add.alpha <- function(col, alpha=1){
+  if(missing(col))
+    stop("Please provide a vector of colours.")
+  apply(sapply(col, col2rgb)/255, 2, 
+                     function(x) 
+                       rgb(x[1], x[2], x[3], alpha=alpha))  
+}
 
 ##--------------------------------------------------
 ## Section to read reaction rate samples
