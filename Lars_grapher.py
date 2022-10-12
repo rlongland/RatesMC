d4b0f66bbfc25719445909da48c16d4fbccb466f
walkelar <81329267+walkelar@users.noreply.github.com>
2022-10-12 07:22:32 -0500

    Add files via upload

--- Lars_grapher.py
+++ Lars_grapher.py
@@ -7,34 +7,56 @@ Created on Tue Aug 23 10:17:12 2022
 """
 
 import matplotlib.pyplot as plt
+import numpy
 
 with open('RatesMC.Lars') as f:
 
     lines = f.readlines()
-    
-    
+
+#Initiallize all the lists for each temp (f for fast, s for slow):
+
+f1 =[]
+f05 = [] 
+f01 = []
+f001 = []
+s1 =[]
+s05 =[]
+s01 = []
+s001 = []
 
 
-
-fast = []
-slow = []
 
 
 for value in lines:
     dummy = value.split(' ')
-    fast.append(float(dummy[0]))
-    slow.append(float(dummy[1]))
+    T = float(dummy[2])
+    if ( T == .1):
+        f1.append(float(dummy[0]))
+        s1.append(float(dummy[1]))
+        
+    elif ( T == .05):
+        f05.append(float(dummy[0]))
+        s05.append(float(dummy[1]))
+        
+    elif ( T == .01):
+        f01.append(float(dummy[0]))
+        s01.append(float(dummy[1]))
+        
+    elif ( T == .001):
+        f001.append(float(dummy[0]))
+        s001.append(float(dummy[1]))
+        
+    
+    
     
   
-difference = []
-total = []
-counter = 0
-    
-for value in fast:
-    diff = value - slow[counter]
-    difference.append(diff)
-    total.append(counter)
-    counter += 1
+def total(f):
+    T = []
+    for num in range(0,len(f)):
+        T.append(num)
+        
+    print (len(T))
+    return T
 
 
 
@@ -46,31 +68,60 @@ def zoom(Y_Points, start, stop):
         zoomed_y.append(Y_Points[num])
         zoomed_x.append(num-start)
     
-    plt.title("Zoomed")    
+     
+    plt.title(f"Zoomed from {start} to {stop}")
+        
+        
     plt.scatter(zoomed_x, zoomed_y)
     plt.show()
-
-   
     
-   
-plt.title("Fast")    
-plt.scatter(total, fast)
-plt.show()
 
-
-plt.title("Slow")
-plt.scatter(total, slow)
-plt.show()
-
-
-plt.title("Fast and Slow")
-plt.scatter(total, slow)
-plt.scatter(total, fast)
-plt.show()
-
-
-zoom(slow, 0, 1000)
+def get_ratio(f,s):
+    R = []
+    
+    for num in range(0,len(f)):
+        ratio = f[num]/s[num]
+        R.append(ratio)
+    return R
 
     
-plt.title("difference")
-plt.scatter(total, difference)
+def Plot(T, index):
+    # num of resonances (constant for this input file), res energy, gamma 1, gamma 2, gamma 3
+    X = []
+    Y = []
+    title =""
+    
+    
+    if ( T == .1):
+        Y= get_ratio(f1, s1)
+        title = "Ratio at T  = .1"
+        
+    elif ( T == .05):
+        Y = get_ratio(f05, s05)
+        title = "Ratio at T  = .05"
+        
+    elif ( T == .01):
+        Y = get_ratio(f01, s01)
+        title = "Ratio at T  = .01"
+        
+    elif ( T == .001):
+        Y = get_ratio(f001, s1)
+        title = "Ratio at T  = .001"
+        
+    if (index == 0):
+        plt.title("No Variable "+title)
+        X = total(Y)
+        
+        
+        
+    plt.scatter(X,Y)
+    plt.show()
+    
+
+
+
+
+Plot(.1,0)
+Plot(.05,0)
+Plot(.01,0)
+Plot(.001,0)
