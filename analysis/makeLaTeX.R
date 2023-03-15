718d593a8b0ea227e9d361bc2788fb08c8881d4a
Richard Longland <rllongla@ncsu.edu>
2023-03-14 20:30:41 -0400

    Added script to make double-column rate table

---
+++ analysis/makeLaTeX.R
@@ -0,0 +1,24 @@
+######################################################################
+## makeLaTeX.R
+## Read in the LaTeX output, reformat the table
+##
+## Author: R. Longland
+## Year  : 2023
+######################################################################
+filename <- "RatesMC.latex"
+filecontent <- readChar(filename, file.info(filename)$size)
+filecontent <- gsub("\n", "", filecontent)
+filecontent <- gsub("\\\\\\\\", "\n", filecontent)
+
+##writeChar(filecontent,"test.dat")
+con <- textConnection(filecontent)
+dat <- read.table(con,sep="&",nrows = 60, colClasses="character")
+
+cut1 <- 1:30
+cut2 <- 31:60
+dat2c <- cbind(dat[cut1,],dat[cut2,])
+
+##dat2c.format <- format(dat2c,nsmall=3)
+
+write.table(file="RatesMC.latex2",dat2c,quote=F,sep=" & ",
+	    eol="  \\\\ \n",col.names=F,row.names=F)
