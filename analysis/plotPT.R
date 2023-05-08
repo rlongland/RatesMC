a698e64567efd73cd79a3cf8e4f2cc9b7c360995
Richard Longland <rllongla@ncsu.edu>
2023-05-08 11:03:05 -0400

    Corrected Porter-Thomas analysis script

--- analysis/plotPT.R
+++ analysis/plotPT.R
@@ -1,8 +1,12 @@
 
 dat <- read.table("RatesMC.PT")
 
-i <- 0
+i <- 14
 j <- 0
+
+## columns in PT file
+##  1          2                3        4          5
+## sample   resonance    Penetration    PTMean     Gamma
 
 myX11 <- function(...) 
 { 
@@ -32,7 +36,7 @@ axis(1,at=aX,labels=aX)
 #,
 #     xlim=range(c(h1$breaks,h2$breaks)),
 #     ylim=range(c(h1$density,h2$density)))
-abline(v=log(1e-3),col="red",lty=2)
+##abline(v=log(1e-3),col="red",lty=2)
 
 plot(h2$mids,h2$density,xlab=paste("Resonance",j),ylab="Freq",
      type='s',xaxt='n')
@@ -42,17 +46,20 @@ axis(1,at=aX,labels=aX)
 #     xlim=range(c(h2$breaks,h2$breaks)),
 #     ylim=range(c(h2$density,h2$density)))
 ##lines(h2$mids,h2$density,col="darkgreen",type='s')
-abline(v=log(4.5e-3),col="red",lty=2)
+##abline(v=log(4.5e-3),col="red",lty=2)
 
-cat("Res: ",i," PT = ",mean(dat[dat[,1]==i,3]),
-    " +/- ",sd(dat[dat[,1]==i,3])," Fac. Unc. = ",
-    sd(dat[dat[,1]==i,3])/mean(dat[dat[,1]==i,3])," \n")
-cat("         median = ",median(dat[dat[,1]==i,3]),"\n")
+fu <- exp(sd(log(dat[cut1,4])))
 
-cat("Res: ",j," PT = ",mean(dat[dat[,1]==j,3]),
-    " +/- ",sd(dat[dat[,1]==j,3])," Fac. Unc. = ",
-    sd(dat[dat[,1]==j,3])/mean(dat[dat[,1]==j,3])," \n")
-cat("         median = ",median(dat[dat[,1]==j,3]),"\n")
+
+cat("Res: ",i," PT = ",mean(dat[cut1,4]),
+    " +/- ",sd(dat[cut1,4])," Fac. Unc. = ",
+    exp(sd(log(dat[cut1,4])))," \n")
+cat("         median = ",median(dat[cut1,4]),"\n")
+
+cat("Res: ",j," PT = ",mean(dat[cut2,4]),
+    " +/- ",sd(dat[cut2,4])," Fac. Unc. = ",
+    exp(sd(log(dat[cut2,4])))," \n")
+cat("         median = ",median(dat[cut2,4]),"\n")
 
 ##----------------------------------------------------------------------
 
@@ -66,33 +73,26 @@ h1 <- hist(d1,plot=F,breaks=b)
 h2 <- hist(d2,plot=F,breaks=b)
 
 plot(h1$mids,h1$density,xlab=paste("Resonance",i),ylab="Freq",
-     type='s',xaxt='n',log='xy',xlim=c(1e-10,10),ylim=c(1e-5,1e10))
+     type='s',xaxt='n',log='xy',xlim=c(1e-6,10),ylim=c(1e-5,1e10))
 aX <- axTicks(1)
 axis(1,at=aX,labels=aX)
 #,
 #     xlim=range(c(h1$breaks,h2$breaks)),
 #     ylim=range(c(h1$density,h2$density)))
-abline(v=4.5e-3,col="red",lty=2)
+##abline(v=4.5e-3,col="red",lty=2)
 med <- median(d1)
 abline(v=med,col="darkgreen",lty=2)
+text(x=med, y=1, "Median", col="darkgreen")
 
 plot(h2$mids,h2$density,xlab=paste("Resonance",j),ylab="Freq",
-     type='s',xaxt='n',log='xy',xlim=c(1e-10,10),ylim=c(1e-5,1e10))
+     type='s',xaxt='n',log='xy',xlim=c(1e-6,10),ylim=c(1e-5,1e10))
 aX <- axTicks(1)
 axis(1,at=aX,labels=aX)
 #,
 #     xlim=range(c(h2$breaks,h2$breaks)),
 #     ylim=range(c(h2$density,h2$density)))
 ##lines(h2$mids,h2$density,col="darkgreen",type='s')
-abline(v=4.5e-3,col="red",lty=2)
+##abline(v=4.5e-3,col="red",lty=2)
 med <- median(d2)
 abline(v=med,col="darkgreen",lty=2)
-
-cat("\n--------------------------------------------------\n")
-cat("Res: ",i," PT = ",mean(dat[dat[,1]==i,4]),
-    " +/- ",sd(dat[dat[,1]==i,4])," \n")
-cat("         median = ",median(dat[dat[,1]==i,4]),"\n")
-
-cat("Res: ",j," PT = ",mean(dat[dat[,1]==j,4]),
-    " +/- ",sd(dat[dat[,1]==j,4])," \n")
-cat("         median = ",median(dat[dat[,1]==j,4]),"\n")
+text(x=med, y=1, "Median", col="darkgreen")
