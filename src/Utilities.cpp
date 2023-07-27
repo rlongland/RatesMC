57699d7f228c27993e2eb05a78abe028e096b089
Richard Longland <rllongla@ncsu.edu>
2023-07-27 11:40:18 -0400

    Q-values are atomic

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -923,7 +923,7 @@ int ReadInputFile(std::string inputfilename, Reaction *R){
 		int zcompound = z0+z1;
 		double mcompound = ame -> readMassFromAandZ(acompound, zcompound);
 		mcompound = atomicToNuclear(mcompound, zcompound);
-		logfile << "= " << mcompound << " (nuclear)\n";
+		logfile << "= " << mcompound << " (atomic)\n";
 		//		std::cout << m0 << "\t" << m1 << "\t" << mcompound << "\t" << AMU << "\n";
 		Qin = ((m0 + m1) - mcompound)*AMU*1000.0;   // To get into keV
 		logfile << "Using 1u = " << AMU << " MeV:\n";
@@ -989,7 +989,7 @@ int ReadInputFile(std::string inputfilename, Reaction *R){
 
 		Qout = ((m2 + mresidual) - mcompound)*AMU*1000.0;   // To get into keV
 		logfile << "Using 1u = " << AMU << " MeV:\n";
-		logfile << "Qout = " << Qout << " keV (nuclear)\n";
+		logfile << "Qout = " << Qout << " keV (atomic)\n";
 		AMEUsed = true;
 	} else {
 		std::cout << "ERROR: Enter a number, 'ame', or 'AME'"
