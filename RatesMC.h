41d1abfe7288dbe31d51321391ad4d9dffdf21d7
Richard Longland <rllongla@ncsu.edu>
2023-08-31 16:08:35 -0400

    Bump version number

--- RatesMC.h
+++ RatesMC.h
@@ -34,8 +34,8 @@ void WelcomeScreen();
 
 
 // Versions
-std::string VersionNumber = "2.2.1";
-std::string VersionDate = "Aug. 29, 2023";
+std::string VersionNumber = "2.2.2";
+std::string VersionDate = "Aug. 31, 2023";
 
 
 #endif
